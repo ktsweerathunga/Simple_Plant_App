ed15ed8df4f4b92480e05b25c82931beecd95cc8
ktsweerathunga <135198812+ktsweerathunga@users.noreply.github.com>
2025-06-26 03:09:51 +0530

    plane cards were made tappable

--- lib/app/widgets/plant_card.dart
+++ lib/app/widgets/plant_card.dart
@@ -4,89 +4,65 @@ class PlantCard extends StatelessWidget {
   final String name;
   final String imageUrl;
   final List<Color> gradient;
+  final VoidCallback? onTap;
 
   const PlantCard({
     super.key,
     required this.name,
     required this.imageUrl,
-    required this.gradient,
+    required this.gradient, 
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 217,
-      height: 328,
-      clipBehavior: Clip.antiAlias,
-      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24.43),
-        ),
-        shadows: const [
-          BoxShadow(
-            color: Color(0x26000000),
-            blurRadius: 70.56,
-            offset: Offset(0, 27.14),
-          )
-        ],
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: Image.network(
-              imageUrl,
-              width: double.infinity,
-              height: 250,
-              fit: BoxFit.cover,
-            ),
+    return GestureDetector(
+      onTap: onTap, // Trigger tap
+      child: Container(
+        width: 217,
+        height: 328,
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24.43),
           ),
-          Positioned(
-            left: 16.28,
-            top: 260.54,
-            child: SizedBox(
-              width: 120,
+          shadows: const [
+            BoxShadow(
+              color: Color(0x26000000),
+              blurRadius: 70.56,
+              offset: Offset(0, 27.14),
+            )
+          ],
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Image.network(
+                imageUrl,
+                width: double.infinity,
+                height: 250,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              left: 16,
+              top: 260,
               child: Text(
                 name,
                 style: const TextStyle(
-                  color: Color(0xFF304022),
-                  fontSize: 16.28,
+                  fontSize: 16,
                   fontFamily: 'Lexend',
                   fontWeight: FontWeight.w600,
+                  color: Color(0xFF304022),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            right: 16,
-            top: 261.90,
-            child: Container(
-              width: 38,
-              height: 36.64,
-              decoration: ShapeDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment(0.50, -0.43),
-                  end: Alignment(0.50, 1.20),
-                  colors: gradient,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24.43),
-                ),
-                shadows: const [
-                  BoxShadow(
-                    color: Color(0x26000000),
-                    blurRadius: 23.07,
-                    offset: Offset(0, 10.86),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
